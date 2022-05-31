c2c97fc0f4145f2c6893c223b88268b44db6ca18
thaiboy001 <100300137+thaiboy001@users.noreply.github.com>
2022-05-31 18:05:16 +0700

    Update main.py

--- src_run_ccminer/main.py
+++ src_run_ccminer/main.py
@@ -1,6 +1,34 @@
 import os, json, time
 from progress.spinner import MoonSpinner
 
+# banner
+setting_banner = """
+███████╗███████╗████████╗████████╗██╗███╗   ██╗ ██████╗ 
+██╔════╝██╔════╝╚══██╔══╝╚══██╔══╝██║████╗  ██║██╔════╝ 
+███████╗█████╗     ██║      ██║   ██║██╔██╗ ██║██║  ███╗
+╚════██║██╔══╝     ██║      ██║   ██║██║╚██╗██║██║   ██║
+███████║███████╗   ██║      ██║   ██║██║ ╚████║╚██████╔╝
+╚══════╝╚══════╝   ╚═╝      ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝"""
+
+running_banner = """
+██████╗ ██╗   ██╗███╗   ██╗         
+██╔══██╗██║   ██║████╗  ██║         
+██████╔╝██║   ██║██╔██╗ ██║         
+██╔══██╗██║   ██║██║╚██╗██║         
+██║  ██║╚██████╔╝██║ ╚████║██╗██╗██╗
+╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝╚═╝"""
+
+# banner function
+def banner(logo):
+    os.system("clear")
+    print(logo,"\nCreated by.mobile-mining")
+    print("---------------------------------------------------") 
+    print("\033[96mสนับสนุนนักพัมนา\033[00m\n"
+        + " กสิกรไทย: 0608905863\n"
+        + "     LTC:  LTT2X57ervCMpfNva5uo1JBzT2UJVBiEZu\n"
+        + "    DOGE:  DFwUqKS3j3RMMkqQF8z6kbYZ652M8VCFVs")
+    print("---------------------------------------------------\n")
+
 # install miner function 
 def install():
     # os.system("git clone --single-branch -b ARM https://github.com/monkins1010/ccminer")
