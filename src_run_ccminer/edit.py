b2c1aabf252c86070994b317ad85c729f1374402
thaiboy001 <100300137+thaiboy001@users.noreply.github.com>
2022-05-31 09:58:26 +0700

    Update edit.py

--- src_run_ccminer/edit.py
+++ src_run_ccminer/edit.py
@@ -20,10 +20,6 @@ def banner(logo):
         + "     LTC:  LTT2X57ervCMpfNva5uo1JBzT2UJVBiEZu\n"
         + "    DOGE:  DFwUqKS3j3RMMkqQF8z6kbYZ652M8VCFVs")
     print("-----------------------------------------------\n")
-    
-    print("-----------------------------------------------")
-    print("\033[96mไฟล์ออฟไลน์ by.FB Sirimekhin Suchaya\033[00m")
-    print("-----------------------------------------------\n")
 
 
 # setting function
