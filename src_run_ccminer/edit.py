57879e196bee2723880a2d54ddbc7362aa774164
thaiboy001 <100300137+thaiboy001@users.noreply.github.com>
2022-05-31 17:23:37 +0700

    Update edit.py

--- src_run_ccminer/edit.py
+++ src_run_ccminer/edit.py
@@ -5,6 +5,7 @@ from progress.spinner import MoonSpinner
 def banner(logo):
     os.system("clear")
     print(logo,"\nCreated by.mobile-mining")
+    print("-------------------------------")
 
 # setting function
 def set_miner():
