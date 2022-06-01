e6dac3ef1bdb35afbd78e59605707a252904490b
thaiboy001 <100300137+thaiboy001@users.noreply.github.com>
2022-06-01 21:20:57 +0700

    Update edit.py

--- src_run_ccminer/edit.py
+++ src_run_ccminer/edit.py
@@ -45,7 +45,7 @@ with MoonSpinner("กําลังเปิดตัวเปลี่ยน�
         for i in range(100):
             time.sleep(0.05)
             bar.next()
-        bar.finish()
+            bar.finish()
 if os.path.exists("set-miner") == True:
     set_miner()
 else:
