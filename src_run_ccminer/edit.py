81ec8315638e10ba19fbab2fd3831dffcdd38964
thaiboy001 <100300137+thaiboy001@users.noreply.github.com>
2022-06-03 21:33:41 +0700

    Update edit.py

--- src_run_ccminer/edit.py
+++ src_run_ccminer/edit.py
@@ -45,7 +45,7 @@ with MoonSpinner("กำลังเรียกใช้ตัวแก้ไ�
         for i in range(100):
             time.sleep(0.05)
             bar.next()
-	bar.finish()           
+os.system("clear")  
 if os.path.exists("set-miner") == True:
     set_miner()
 else:
